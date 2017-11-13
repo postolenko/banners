9ed5aad82c8d22b54fab029075ef0a1bb0f6c427
postolenko <denis.postolenko@gmail.com>
2017-11-13 03:13:41 +0200

    Banners

--- 300x250/js/scripts.js
+++ 300x250/js/scripts.js
@@ -7,16 +7,6 @@ $(document).ready(function() {
 
     // ----------------------------
 
-    var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
-    // ----------------------------
-
-    getImgPosition();
-
     // getFooterPosition();
 
     $(window).resize(function() {
@@ -29,74 +19,14 @@ $(document).ready(function() {
 
         // }
 
-        bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
-        getImgPosition();
-
     });
 
 
     $(function() {
 
-        var topCoord;
-        var bannerHeader;
-
-        $( ".banner_300_250").bind({
-          mouseenter: function() {
-
-            bannerMask = $(this).find(".mask");
-            bannerMask.attr("style", "top: "+ 0 +"px");
-
-          },
-          mouseleave: function() {            
-
-            bannerMask = $(this).find(".mask");
-            topCoord = $(this).height() - $(this).find(".banner-header").outerHeight();
-            bannerMask.attr("style", "top: "+ topCoord +"px");  
-
-          }
-        });
+        
 
     });
-
-    function getImgPosition() {
-
-        if( bodyWidth <= 480 ) {
-
-            $(".banner_300_250").each(function() {
-
-                var photoBanner = $(this).find(".img-box");
-
-                var bannerHeader = $(this).find(".banner-header");
-
-                bannerHeader.css({
-                    "height" : "auto"
-                });
-
-                photoBanner.css({
-                    "top" : bannerHeader.height() + "px"
-                });
-
-            });
-
-        } else {
-
-            $(".banner_300_250").find(".img-box").css({
-                "top" : 0
-            });
-
-            $(".banner_300_250").each(function() {
-
-                bannerMask = $(this).find(".mask");
-                $(this).find(".banner-header").css({"height" : "auto"});
-                topCoord = $(this).height() - $(this).find(".banner-header").outerHeight();
-                bannerMask.attr("style", "top: "+ topCoord +"px");
-
-            });
-
-        }
-
-    }
 
 
     // function getFooterPosition() {
