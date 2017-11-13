41c4ee0ebb0b16013758f9c0b516eb089b9980fd
postolenko <denis.postolenko@gmail.com>
2017-11-13 03:08:10 +0200

    Banners

--- 300x250/js/scripts.js
+++ 300x250/js/scripts.js
@@ -41,20 +41,12 @@ $(document).ready(function() {
         var topCoord;
         var bannerHeader;
 
-        $(".banner_300_250").each(function() {
-
-            bannerMask = $(this).find(".mask");
-            $(this).find(".banner-header").css({"height" : "auto"});
-            topCoord = $(this).height() - $(this).find(".banner-header").outerHeight();
-            bannerMask.attr("style", "top: "+ topCoord +"px");
-
-        });
-
         $( ".banner_300_250").bind({
           mouseenter: function() {
 
             bannerMask = $(this).find(".mask");
             bannerMask.attr("style", "top: "+ 0 +"px");
+
           },
           mouseleave: function() {            
 
@@ -77,6 +69,10 @@ $(document).ready(function() {
 
                 var bannerHeader = $(this).find(".banner-header");
 
+                bannerHeader.css({
+                    "height" : "auto"
+                });
+
                 photoBanner.css({
                     "top" : bannerHeader.height() + "px"
                 });
@@ -86,8 +82,15 @@ $(document).ready(function() {
         } else {
 
             $(".banner_300_250").find(".img-box").css({
-
                 "top" : 0
+            });
+
+            $(".banner_300_250").each(function() {
+
+                bannerMask = $(this).find(".mask");
+                $(this).find(".banner-header").css({"height" : "auto"});
+                topCoord = $(this).height() - $(this).find(".banner-header").outerHeight();
+                bannerMask.attr("style", "top: "+ topCoord +"px");
 
             });
 
@@ -96,34 +99,34 @@ $(document).ready(function() {
     }
 
 
-    function getFooterPosition() {
+    // function getFooterPosition() {
 
-        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+    //     $(".wrapper").css({"min-height" : $(window).height() + "px"});
 
-        if($(".footer").length > 0) {
+    //     if($(".footer").length > 0) {
 
-            $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+    //         $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
 
-            setFooterPositionInterval = setInterval(function() {
+    //         setFooterPositionInterval = setInterval(function() {
 
-                contentCoor = $(".content").offset().top + $(".content").height();
-                footerCoor = $(".footer").offset().top;
+    //             contentCoor = $(".content").offset().top + $(".content").height();
+    //             footerCoor = $(".footer").offset().top;
 
-                if( contentCoor != footerCoor ) {
+    //             if( contentCoor != footerCoor ) {
 
-                    $(".wrapper").css({"min-height" : $(window).height() + "px"});
+    //                 $(".wrapper").css({"min-height" : $(window).height() + "px"});
 
-                    $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+    //                 $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
 
-                    clearInterval(setFooterPositionInterval);
+    //                 clearInterval(setFooterPositionInterval);
 
-                }
+    //             }
 
-            }, 35);
+    //         }, 35);
 
-        }
+    //     }
 
-    }
+    // }
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # http://postolenko.github.io/banners/250x200/index.html
 # http://postolenko.github.io/banners/260x260/index.html
+# http://postolenko.github.io/banners/300x250/index.html
